ca29ed580afbdcc60b851119b4a7b3a8e0f83411
Jeffro <0xjeffro@gmail.com>
2024-12-25 02:49:43 -0800

    bug fixed

--- parser/parser.go
+++ parser/parser.go
@@ -3,12 +3,9 @@ package parser
 import (
 	"encoding/json"
 	"github.com/0xjeffro/tx-parser/solana"
-	"github.com/0xjeffro/tx-parser/solana/programs/pumpfun"
 	pumpfunParser "github.com/0xjeffro/tx-parser/solana/programs/pumpfun/parsers"
 	systemParser "github.com/0xjeffro/tx-parser/solana/programs/systemProgram/parsers"
 	"github.com/0xjeffro/tx-parser/solana/types"
-	"github.com/mr-tron/base58"
-
 	"log"
 )
 
@@ -41,22 +38,28 @@ func Parser(bytes []byte) ([]types.Action, *types.ParsedResult, error) {
 		programID := parsedResult.AccountList[instr.ProgramIDIndex]
 		switch programID {
 		case PumpFunProgramID:
-			decodedData, err := base58.Decode(instr.Data)
-			if err != nil {
-				log.Println("[Parser] Decode error: ", err)
-				continue
-			}
-			discriminator := *(*[16]byte)(decodedData[:16])
-			mergedDiscriminator := make([]byte, 0, 16)
-			mergedDiscriminator = append(mergedDiscriminator[:], pumpfun.AnchorSelfCPILogDiscriminator[:]...)
-			mergedDiscriminator = append(mergedDiscriminator[:], pumpfun.AnchorSelfCPILogSwapDiscriminator[:]...)
-			if discriminator == *(*[16]byte)(mergedDiscriminator[:]) {
-				action, err := pumpfunParser.AnchorSelfCPILogSwapParser(decodedData)
-				if err == nil {
-					actions = append(actions, action)
-				} else {
-					log.Println("[Parser] PumpFun instruction error: ", err)
-				}
+			//decodedData, err := base58.Decode(instr.Data)
+			//if err != nil {
+			//	log.Println("[Parser] Decode error: ", err)
+			//	continue
+			//}
+			//discriminator := *(*[16]byte)(decodedData[:16])
+			//mergedDiscriminator := make([]byte, 0, 16)
+			//mergedDiscriminator = append(mergedDiscriminator[:], pumpfun.AnchorSelfCPILogDiscriminator[:]...)
+			//mergedDiscriminator = append(mergedDiscriminator[:], pumpfun.AnchorSelfCPILogSwapDiscriminator[:]...)
+			//if discriminator == *(*[16]byte)(mergedDiscriminator[:]) {
+			//	action, err := pumpfunParser.AnchorSelfCPILogSwapParser(decodedData)
+			//	if err == nil {
+			//		actions = append(actions, action)
+			//	} else {
+			//		log.Println("[Parser] PumpFun instruction error: ", err)
+			//	}
+			//}
+			action, err := pumpfunParser.InstructionRouter(&parsedResult, instr)
+			if err == nil {
+				actions = append(actions, action)
+			} else {
+				log.Println("[Parser] PumpFun instruction error: ", err)
 			}
 		case SystemProgramID:
 			action, err := systemParser.InstructionRouter(&parsedResult, instr)
